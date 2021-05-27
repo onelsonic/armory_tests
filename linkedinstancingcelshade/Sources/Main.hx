d52e82df171391c215b9d0923bf176a35db799d8
onelsonic <onelsonicmessageme@ongithub.com>
2021-05-27 23:34:22 +0200

    Initial commit

--- linkedinstancingcelshade/Sources/Main.hx
+++ linkedinstancingcelshade/Sources/Main.hx
@@ -2,10 +2,12 @@
 package ;
 class Main {
     public static inline var projectName = 'scene';
-    public static inline var projectVersion = '1.0.6';
+    public static inline var projectVersion = '1.0.7';
     public static inline var projectPackage = 'arm';
     public static function main() {
         iron.object.BoneAnimation.skinMaxBones = 8;
+        armory.system.Starter.numAssets = 7;
+        armory.system.Starter.drawLoading = armory.trait.internal.LoadingScreen.render;
         armory.system.Starter.main(
             'Scene',
             0,
