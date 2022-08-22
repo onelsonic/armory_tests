6ee4894e77b55bd1014332cfe77b46123030128d
onelsonic <onelsonicmessageme@ongithub.com>
2022-08-22 22:02:35 +0200

    Initial commit

---
+++ cubes/Sources/Main.hx
@@ -0,0 +1,25 @@
+// Auto-generated
+package ;
+class Main {
+    public static inline var projectName = 'june08cube';
+    public static inline var projectVersion = '1.0.1';
+    public static inline var projectPackage = 'arm';
+    public static inline var resolutionSize = 430;
+    public static function main() {
+        iron.object.BoneAnimation.skinMaxBones = 8;
+        armory.system.Starter.numAssets = 3;
+        armory.system.Starter.drawLoading = armory.trait.internal.LoadingScreen.render;
+        armory.system.Starter.main(
+            'Scene',
+            0,
+            false,
+            true,
+            false,
+            800,
+            600,
+            1,
+            true,
+            armory.renderpath.RenderPathCreator.get
+        );
+    }
+}
---
+++ cubes/khafile.js
@@ -0,0 +1,34 @@
+// Auto-generated
+let project = new Project('june08cube_1_0_1');
+
+project.addSources('Sources');
+project.addLibrary("C:/ArmorySDK2208_latest/ArmorySDK2208/armory");
+project.addLibrary("C:/ArmorySDK2208_latest/ArmorySDK2208/iron");
+project.addParameter('-dce full');
+project.addParameter('armory.trait.internal.UniformsManager');
+project.addParameter("--macro keep('armory.trait.internal.UniformsManager')");
+project.addShaders("build_june08cube/compiled/Shaders/*.glsl", { noembed: false});
+project.addAssets("build_june08cube/compiled/Assets/**", { notinlist: true });
+project.addAssets("build_june08cube/compiled/Shaders/*.arm", { notinlist: true });
+project.addDefine('rp_renderer=Forward');
+project.addDefine('rp_shadowmap');
+project.addDefine('rp_shadowmap_cascade=1024');
+project.addDefine('rp_shadowmap_cube=256');
+project.addDefine('rp_background=Clear');
+project.addDefine('rp_render_to_texture');
+project.addDefine('rp_antialiasing=Off');
+project.addDefine('rp_supersampling=1');
+project.addDefine('rp_ssgi=Off');
+project.addDefine('rp_resolution_filter=Linear');
+project.addDefine('arm_published');
+project.addDefine('arm_soundcompress');
+project.addDefine('arm_audio');
+project.addDefine('arm_skin');
+project.addDefine('arm_morph_target');
+project.addDefine('arm_particles');
+project.addDefine('arm_loadscreen');
+project.addDefine('arm_resizable');
+project.addDefine('armory');
+
+
+resolve(project);
